b5a7211c9fa3ce40162f595b1c287d0159b1ef63
saimurswarnab <saimur101@gmail.com>
2022-12-27 01:12:23 +0600

    Commit

---
+++ app/src/main/java/com/example/intent01/secondActivaty.java
@@ -0,0 +1,14 @@
+package com.example.intent01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secondActivaty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_activaty);
+    }
+}
